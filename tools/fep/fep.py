40573fbfe15c906fdc383ed1792e4e562f74ad00
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2024-06-07 12:52:54 +0200

    A beautified version of fep.py

--- tools/fep/fep.py
+++ tools/fep/fep.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 # fep.py - calculate free energy from compute fep results
 
-import sys
+from argparse import ArgumentParser
 import math
 
-if len(sys.argv) < 2:
-    print("Free Energy Perturbation")
-    print("usage: fep.py temperature < out.fep")
-    sys.exit()
+def compute_fep():
 
-rt = 0.008314 / 4.184 * float(sys.argv[1]) # in kcal/mol
+    parser = ArgumentParser(description='A python script to calculate free energy from compute fep results')
 
-v = 1.0
-sum = 0.0
-for line in sys.stdin:
-    if line.startswith("#"):
-        continue
-    tok = line.split()
-    if len(tok) == 4:
-        v = float(tok[3])
-    sum += math.log(float(tok[2]) / v)
+    parser.add_argument("Temperature", type=float, help="The temperature of the system in Kelvin")
+    parser.add_argument("InputFile", help="The name of the file with the output from compute fep.")
 
-print(-rt * sum)
+    args = parser.parse_args()
+
+    rt = 0.008314 / 4.184 * args.Temperature # in kcal/mol
+
+    v = 1.0
+    sum = 0.0
+
+    with open(args.InputFile, "r") as input_file:
+        for line in input_file:
+            if line.startswith("#"):
+                continue
+            tok = line.split()
+            if len(tok) == 4:
+                v = float(tok[3])
+            sum += math.log(float(tok[2]) / v)
+
+    print(-rt * sum)
+
+if __name__ == "__main__":
+    compute_fep()
+
